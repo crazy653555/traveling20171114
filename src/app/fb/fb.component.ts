48de75ee9be0b16da2fc9b1ecfe692c12910b23f
eason <crazy653555@gmail.com>
2017-11-14 13:10:40 +0800

    修改內容

--- src/app/fb/fb.component.ts
+++ src/app/fb/fb.component.ts
@@ -14,34 +14,39 @@ export class FbComponent implements OnInit {
       name:'果丁莓 ‧ 蹦跳鳥日子',
       content:'熱愛攝影的鳥媽咪，紀錄著兩隻肉桂小太陽『芒果Mango』、『布丁Pudding』和一隻藍化鳳梨小太陽『藍莓 Blueberry』的蹦蹦跳跳鳥日子～╰( ◕ ◇ ◕ )╯',
       link:'https://www.facebook.com/DelightedParrots/',
+      like:'6,342',
       img:'./assets/img/gdm.png'
     },
     {
       id:2,
-     name:'果丁莓 ‧ 蹦跳鳥日子',
-      content:'熱愛攝影的鳥媽咪，紀錄著兩隻肉桂小太陽『芒果Mango』、『布丁Pudding』和一隻藍化鳳梨小太陽『藍莓 Blueberry』的蹦蹦跳跳鳥日子～╰( ◕ ◇ ◕ )╯',
-      link:'https://www.facebook.com/DelightedParrots/',
+     name:'莉。莉。公寓',
+      content:'一個鳥奴媽媽因為不想出去工作時間很常陪鳥寶的時間很短，而決定成立點心工作室自己接單上班還可以陪鳥是有沒有這麼任性',
+      link:'https://www.facebook.com/%E8%8E%89%E8%8E%89%E5%85%AC%E5%AF%93-618808951482973/',
+      like:'1,315',
       img:'./assets/img/gdm.png'
     },
     {
       id:3,
-     name:'果丁莓 ‧ 蹦跳鳥日子',
-      content:'熱愛攝影的鳥媽咪，紀錄著兩隻肉桂小太陽『芒果Mango』、『布丁Pudding』和一隻藍化鳳梨小太陽『藍莓 Blueberry』的蹦蹦跳跳鳥日子～╰( ◕ ◇ ◕ )╯',
-      link:'https://www.facebook.com/DelightedParrots/',
+     name:'拉吉歐x豆子們x生活塗鴉',
+      content:'四顆豆子+一隻拉吉歐與媽媽的故事',
+      link:'https://www.facebook.com/Rajio.mame/',
+      like:'265',
       img:'./assets/img/gdm.png'
     },
     {
       id:4,
-     name:'果丁莓 ‧ 蹦跳鳥日子',
-      content:'熱愛攝影的鳥媽咪，紀錄著兩隻肉桂小太陽『芒果Mango』、『布丁Pudding』和一隻藍化鳳梨小太陽『藍莓 Blueberry』的蹦蹦跳跳鳥日子～╰( ◕ ◇ ◕ )╯',
-      link:'https://www.facebook.com/DelightedParrots/',
+     name:'英雄鳥聯盟',
+      content:'媽覺得我們太可愛了所以要創粉絲專業',
+      link:'https://www.facebook.com/pg/%E8%8B%B1%E9%9B%84%E9%B3%A5%E8%81%AF%E7%9B%9F-637192539689225/about/?ref=page_internal',
+      like:'85',
       img:'./assets/img/gdm.png'
     },
     {
       id:5,
-     name:'果丁莓 ‧ 蹦跳鳥日子',
-      content:'熱愛攝影的鳥媽咪，紀錄著兩隻肉桂小太陽『芒果Mango』、『布丁Pudding』和一隻藍化鳳梨小太陽『藍莓 Blueberry』的蹦蹦跳跳鳥日子～╰( ◕ ◇ ◕ )╯',
-      link:'https://www.facebook.com/DelightedParrots/',
+     name:'Pinky & Halfy',
+      content:'Pinky媽 & Halfy爸 紀錄鳥寶的日常',
+      link:'https://www.facebook.com/pg/PinkyHalfy/about/?ref=page_internal',
+      like:'30',
       img:'./assets/img/gdm.png'
     },
   ]
--- src/app/ig/ig.component.ts
+++ src/app/ig/ig.component.ts
@@ -11,38 +11,51 @@ export class IgComponent implements OnInit {
   data = [
     {
       id:1,
-      name:'果丁莓 ‧ 蹦跳鳥日子',
-      content:'熱愛攝影的鳥媽咪，紀錄著兩隻肉桂小太陽『芒果Mango』、『布丁Pudding』和一隻藍化鳳梨小太陽『藍莓 Blueberry』的蹦蹦跳跳鳥日子～╰( ◕ ◇ ◕ )╯',
-      link:'https://www.facebook.com/DelightedParrots/',
-      img:'./assets/img/gdm.png'
+      name:'delighted_parrots',
+      content:'果丁莓 & YJ 『果丁莓 ‧ 蹦跳鳥日子』三隻小屁鳥的生活點滴，還有『YJ。傻笑看人生』媽咪人生中遇見鳥的日子 =))',
+      link:'https://www.instagram.com/delighted_parrots/',
+      like:'575',
+      img:'./assets/img/gdm-ig.png'
     },
     {
       id:2,
-     name:'果丁莓 ‧ 蹦跳鳥日子',
-      content:'熱愛攝影的鳥媽咪，紀錄著兩隻肉桂小太陽『芒果Mango』、『布丁Pudding』和一隻藍化鳳梨小太陽『藍莓 Blueberry』的蹦蹦跳跳鳥日子～╰( ◕ ◇ ◕ )╯',
-      link:'https://www.facebook.com/DelightedParrots/',
-      img:'./assets/img/gdm.png'
+     name:'Delilahwong',
+      content:'Delilah Wong The past and present exist for the future Toast To life。 過去成就未來，把握當下，紀錄每一天',
+      link:'https://www.instagram.com/delilahwong/',
+      like:'615',      
+      img:'./assets/img/delilahwong-ig.png'
     },
     {
       id:3,
-     name:'果丁莓 ‧ 蹦跳鳥日子',
+     name:'Nekoko',
       content:'熱愛攝影的鳥媽咪，紀錄著兩隻肉桂小太陽『芒果Mango』、『布丁Pudding』和一隻藍化鳳梨小太陽『藍莓 Blueberry』的蹦蹦跳跳鳥日子～╰( ◕ ◇ ◕ )╯',
       link:'https://www.facebook.com/DelightedParrots/',
-      img:'./assets/img/gdm.png'
+      like:'114',      
+      img:'./assets/img/gdm-ig.png'
     },
     {
       id:4,
-     name:'果丁莓 ‧ 蹦跳鳥日子',
-      content:'熱愛攝影的鳥媽咪，紀錄著兩隻肉桂小太陽『芒果Mango』、『布丁Pudding』和一隻藍化鳳梨小太陽『藍莓 Blueberry』的蹦蹦跳跳鳥日子～╰( ◕ ◇ ◕ )╯',
-      link:'https://www.facebook.com/DelightedParrots/',
-      img:'./assets/img/gdm.png'
+     name:'My6birds',
+      content:'Y-H Wu My 4 greencheek conures🐧(Marvel🚹Stark🚹Packer🚹Groot🚺),and 2 lovebirds 🐤（Hulk🚹Thor🚹）我的四隻綠頰小太陽和兩隻愛情鳥',
+      link:'https://www.instagram.com/my6birds/',
+      like:'306',
+      img:'./assets/img/my6birds-ig.png'
     },
     {
       id:5,
-     name:'果丁莓 ‧ 蹦跳鳥日子',
-      content:'熱愛攝影的鳥媽咪，紀錄著兩隻肉桂小太陽『芒果Mango』、『布丁Pudding』和一隻藍化鳳梨小太陽『藍莓 Blueberry』的蹦蹦跳跳鳥日子～╰( ◕ ◇ ◕ )╯',
-      link:'https://www.facebook.com/DelightedParrots/',
-      img:'./assets/img/gdm.png'
+     name:'yanlin.chou',
+      content:'Yanlin Chou 小太陽鸚鵡G',
+      link:'https://www.instagram.com/yanlin.chou/',
+      like:'103',      
+      img:'./assets/img/yanlinchou-ig.png'
+    },
+        {
+      id:5,
+     name:'Emilia Liao',
+      content:'Emilia Liao 鳥寶日常。小太陽-林包肉Paul；玄鳳-綿綿醬John；文鳥-叭噗',
+      link:'https://www.instagram.com/lin_bao_zo/',
+      like:'52',      
+      img:'./assets/img/lin_ban_zo-ig.png'
     },
   ]
 
